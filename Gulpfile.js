64811c87d0eec37a5f66b78440487992806f8089
Steffen Müller <steffen@mllrsohn.com>
2014-01-18 18:45:04 +0100

    pr note, gulp release

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var karma = require('gulp-karma');
+var bump = require('gulp-bump');
+var git = require('gulp-git');
 
+// Testing
 gulp.task('test', function() {
     gulp.src([
         'test/mocha.conf.js',
@@ -16,4 +19,24 @@ gulp.task('test', function() {
         browsers: (gulp.env.travis ? ['Firefox'] : ['Chrome']),
         singleRun: (gulp.env.travis ? true : false),
     }));
+});
+
+// Dump the version
+gulp.task('bump', function() {
+    gulp.src('./package.json')
+        .pipe(bump())
+        .pipe(gulp.dest('./'));
+
+    gulp.src('./bower.json')
+        .pipe(bump())
+        .pipe(gulp.dest('./'));
+
+});
+
+// Tag the repo with a version
+gulp.task('tag', function() {
+    var pkg = require('./package.json');
+    var version = 'v' + pkg.version;
+    gulp.src('./')
+        .pipe(git.tag(version, 'Release ('+version+')'));
 });
--- README.md
+++ README.md
@@ -55,6 +55,9 @@ Default: `null`
 
 Sets the options, that get passed into the Recaptcha.create call. Here are a list of the [available options](https://developers.google.com/recaptcha/docs/customization)
 
+## Contribute
+Pull requests are welcome. Please make sure that you include tests in your PR.
+
 ## License
 
 [MIT License](http://en.wikipedia.org/wiki/MIT_License)
